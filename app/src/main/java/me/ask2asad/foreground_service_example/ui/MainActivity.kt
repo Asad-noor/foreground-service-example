65a991576086d58e21b1a190b072e123538182e8
Md. Asaduzzaman Noor <md.noor.asad@gmail.com>
2019-11-28 21:46:55 +0600

    Add hour minute picker and attach with viewModel.

--- app/src/main/java/me/ask2asad/foreground_service_example/ui/MainActivity.kt
+++ app/src/main/java/me/ask2asad/foreground_service_example/ui/MainActivity.kt
@@ -1,35 +1,42 @@
 package me.ask2asad.foreground_service_example.ui
 
+import android.app.TimePickerDialog
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-
 import kotlinx.android.synthetic.main.activity_main.*
 import me.ask2asad.foreground_service_example.R
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         setSupportActionBar(toolbar)
+        init()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+    private fun init() {
+        btnSetAlarm.setOnClickListener {
+            showDateTimePicker()
         }
+    }
+
+    private fun showDateTimePicker() {
+        val mcurrentTime = Calendar.getInstance()
+        val hour = mcurrentTime.get(Calendar.HOUR_OF_DAY)
+        val minute = mcurrentTime.get(Calendar.MINUTE)
+        val mTimePicker: TimePickerDialog
+        mTimePicker = TimePickerDialog(this,
+            TimePickerDialog.OnTimeSetListener { timePicker, selectedHour, selectedMinute ->
+                edtDateTime.setText(
+                    "$selectedHour:$selectedMinute"
+                )
+                edtDateTime.setSelection(edtDateTime.text!!.length)
+            }, hour, minute, true
+        )//Yes 24 hour time
+        mTimePicker.setTitle("Select Time")
+        mTimePicker.show()
     }
 }
--- app/src/main/java/me/ask2asad/foreground_service_example/ui/MainActivityViewModel.kt
+++ app/src/main/java/me/ask2asad/foreground_service_example/ui/MainActivityViewModel.kt
@@ -1,6 +1,11 @@
 package me.ask2asad.foreground_service_example.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.databinding.adapters.TextViewBindingAdapter.setText
+import android.widget.TimePicker
+import android.app.TimePickerDialog
+import java.util.*
+
 
 class MainActivityViewModel: ViewModel() {
 
